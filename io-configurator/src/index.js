6d498f48979256c152f5ed8cb402951a7b3c49a2
cyann <cyann74@gmail.com>
2019-08-14 11:49:52 +0200

    configure rete with dependencies

---
+++ io-configurator/src/index.js
@@ -0,0 +1,44 @@
+
+import "@babel/polyfill";
+import Rete from "rete";
+import ConnectionPlugin from 'rete-connection-plugin';
+import VueRenderPlugin from 'rete-vue-render-plugin';
+
+const numSocket = new Rete.Socket('Number value');
+
+class NumComponent extends Rete.Component {
+    constructor() {
+        super('Number');
+    }
+
+    builder(node) {
+        const out = new Rete.Output('num', 'Number', numSocket);
+
+        node.addOutput(out);
+    }
+
+    worker(node, inputs, outputs) {
+        outputs['num'] = node.data.num;
+    }
+}
+
+// init the editor
+
+const container = document.querySelector('#rete');
+const editor = new Rete.NodeEditor('demo@0.1.0', container);
+
+editor.use(ConnectionPlugin)
+editor.use(VueRenderPlugin)
+
+const numComponent = new NumComponent();
+editor.register(numComponent);
+
+// visualize
+
+const engine = new Rete.Engine('demo@0.1.0');
+engine.register(numComponent);
+
+editor.on('process nodecreated noderemoved connectioncreated connectionremoved', async () => {
+    await engine.abort();
+    await engine.process(editor.toJSON());
+});
