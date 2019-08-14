bede8ff81b40cd28e99e684ebb8e0711797ac8fa
cyann <cyann74@gmail.com>
2019-08-14 09:39:23 +0200

    webpack configuration base

---
+++ io-configurator/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    watch: true,
+    mode: "none",
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js"
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, "dist"),
+        writeToDisk: true,
+        open: true
+    },
+    module: {
+        rules: [
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'public/index.html')
+        })
+    ]
+}
